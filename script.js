5c8ab6e8b4c2b26518625fa7651ee34f63eb882c
Fabiha517 <fabihahassan0@gmail.com>
2025-08-26 19:57:17 +0500

    Layout Completed

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 function showMenu() {
-	const menu = document.querySelector(".menu");
-	const menusvg = document.querySelector(".menusvg");
-	const menuOp = document.createElement("div");
-	menuOp.className = "menuOp";
-	menuOp.style.display = "none";
-	const clonedItems = menu.cloneNode(true);
-	menusvg.addEventListener("click", () => {
-		menuOp.style.display = "block";
-	});
-	menuOp.appendChild(clonedItems);
-	document.body.appendChild(menuOp);
-    window.addEventListener("scroll", () => {
-		menuOp.style.display = "none";
-	});
-	document.addEventListener("click", (e) => {
-		if (!menuOp.contains(e.target) && !menusvg.contains(e.target)) {
-			menuOp.style.display = "none";
-		}
-	});
-
+  const menu = document.querySelector(".menu");
+  const menusvg = document.querySelector(".menusvg");
+  const menuOp = document.createElement("div");
+  menuOp.className = "menuOp";
+  menuOp.style.display = "none";
+  const clonedItems = menu.cloneNode(true);
+  menusvg.addEventListener("click", () => {
+    menuOp.style.display = "block";
+  });
+  menuOp.appendChild(clonedItems);
+  document.body.appendChild(menuOp);
+  window.addEventListener("scroll", () => {
+    menuOp.style.display = "none";
+  });
+  document.addEventListener("click", (e) => {
+    if (!menuOp.contains(e.target) && !menusvg.contains(e.target)) {
+      menuOp.style.display = "none";
+    }
+  });
 }
 showMenu();
+function EnterUrl() {
+  const input = document.querySelector(".inputBox input");
+  const ShortenBtn = document.querySelector(".shortenIt");
+  const inputBox = document.querySelector(".inputBox");
+
+  const alert = document.querySelector(".alertMsg");
+  alert.textContent="Please add a link"
+  alert.style.display = "none";
+  ShortenBtn.addEventListener("click", () => {
+    if (input.value.trim() === "") {
+      input.classList.add("error");
+      input.style.border = "2px solid rgba(255, 20, 0, 0.9)";
+      alert.style.display = "flex";
+    } else {
+      input.classList.remove("error"); // resets to normal
+      input.style.border = "";
+    }
+  });
+}
+EnterUrl();
