ecaa79a293bec1f892d986af0e94d490c9786fb8
Fabiha517 <fabihahassan0@gmail.com>
2025-08-27 16:48:39 +0500

    Fetched API

--- script.js
+++ script.js
@@ -25,20 +25,69 @@ function EnterUrl() {
   const ShortenBtn = document.querySelector(".shortenIt");
   const inputBox = document.querySelector(".inputBox");
 
-  const alert = document.createElement("div")
-  alert.className="alertMsg"
-  alert.textContent="Please add a link"
-  
+  const alert = document.createElement("div");
+  alert.className = "alertMsg";
+  alert.textContent = "Please add a link";
+
   ShortenBtn.addEventListener("click", () => {
     if (input.value.trim() === "") {
       input.classList.add("error");
       input.style.border = "2px solid rgba(255, 20, 0, 0.9)";
       alert.style.display = "flex";
-      input.insertAdjacentElement("afterend",alert)
+      input.insertAdjacentElement("afterend", alert);
     } else {
       input.classList.remove("error"); // resets to normal
       input.style.border = "";
+      alert.style.display = "none";
     }
   });
 }
 EnterUrl();
+
+async function shortenUrl(longurl) {
+  try {
+    const proxy = "https://corsproxy.io/?";
+    // The real API
+    const apiUrl = "https://cleanuri.com/api/v1/shorten";
+    // encodeURIComponent(API_URL) → makes the API URL safe to pass as a query string so that it does not gets mixed up with the proxy string
+    const response = await fetch(proxy + encodeURIComponent(apiUrl), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `url=${encodeURIComponent(longurl)}`,
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+  } catch (error) {
+    // console.warn(`${proxy} failed:`, error);
+    alert("❌ Failed to shorten URL");
+  }
+}
+
+const shortenBtn = document
+  .querySelector(".shortenIt")
+  .addEventListener("click", async () => {
+    const userInput = document.querySelector(".urlInput").value.trim();
+
+    const response = await shortenUrl(userInput);
+    // result_url converts the object into string
+    const shortUrl = response.result_url;
+
+    const result = document.createElement("div");
+    result.className = "result";
+    result.innerHTML = ` 
+    <div class="userInput">${userInput}</div>
+    <div class="linkBtn"><div class="shortenedUrl"><a href="${shortUrl}" target="_blank">${shortUrl}</a> </div>
+    <button class="copy">Copy</button></div>
+
+    
+    `;
+
+    const resultBox = document.querySelector(".resultBox");
+    resultBox.appendChild(result);
+  });
